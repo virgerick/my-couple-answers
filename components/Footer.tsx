84c65be58fcb8dcf6a84b3b00b72e58671a93e39
Heriberto Alcantara Luna <37025337+virgerick@users.noreply.github.com>
2021-09-20 08:18:56 -0400

    footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,21 @@
 import React, { ReactElement } from "react";
+import style from '../styles/Layout.module.css'
 
 interface Props {}
 
 export default function Footer({}: Props): ReactElement {
   return (
-    <footer className="">
-    </footer>
+    <footer className={styles.footer}>
+    <a
+      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Powered by{" "}
+      <span className={styles.logo}>
+        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      </span>
+    </a>
+  </footer>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = (props) => {
 
   return (
     <div className={styles.container}>
-   
+
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -50,19 +50,7 @@ const Home: NextPage = (props) => {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      
     </div>
   );
 };
