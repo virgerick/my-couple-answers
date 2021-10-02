bdc27c7010185dc2453e8e17150e8e4e64b7a944
HERIBERTO ALCANTARA <elfeo.0103@gmail.com>
2021-10-02 11:07:06 -0400

    test collection firestore

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,21 @@
 import React, { ReactElement } from "react";
-import { Nav } from "react-bootstrap";
+import { Container, Nav } from "react-bootstrap";
 
-interface Props { }
+interface Props {}
 
-export default function Footer({ }: Props): ReactElement {
+export default function Footer({}: Props): ReactElement {
   return (
-    <footer  className="fixed-bottom py-3 bg-light" >
-    <Nav.Link
-      href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Powered by {"Heriberto Alcantara Luna "}
-      
-    </Nav.Link>
-  </footer>
+    <footer className="fixed-bottom py-3 bg-light">
+      <Nav>
+        <Container>
+          <Nav.Link
+            href="https://github.com/virgerick"
+            target="_blank"
+          >
+            Powered by <strong>Heriberto Alcantara Luna</strong>
+          </Nav.Link>
+        </Container>
+      </Nav>
+    </footer>
   );
 }
--- pages/api/questions/index.ts
+++ pages/api/questions/index.ts
@@ -1,18 +1,62 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import firebase from "../../../firebase";
+
 import type { NextApiRequest, NextApiResponse } from "next";
-
-type Data = {
-  name: string[];
+import HttpStatusCode from "../../../enums/HttpStatusCode";
+import HTTPMethod from "../../../enums/HTTPMethod";
+const { GET, POST, PUT } = HTTPMethod;
+type Response = {
+  message?: string | string[];
+  success: boolean;
+  result?: Question | any;
 };
-
-export default function handler(
+export interface Question {
+  id: string;
+  name: string;
+  answars: Array<string>;
+}
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Response>
 ) {
-    
-    const {method,body,query}=req
-    console.log({method,body,query});
-    
-    
-  res.status(200).json({ name: ["John Doe","hello "]  });
+  try {
+    const { method, body, query } = req;
+    console.log({ method, body, query });
+
+    const db = firebase?.firestore();
+    const votesRef = db.collection("votes");
+    switch (method) {
+      case GET:
+        let data: Array<Question> = [];
+        if (!query.vote && !query.id) {
+          const snapshot = await votesRef.get();
+          data = snapshot.docs.map((x) => x.data() as Question);
+        } else {
+          const snapshot = await votesRef.where("vote", "==", query.vote).get();
+          data = snapshot.docs.map((x) => x.data() as Question);
+        }
+
+        res.status(HttpStatusCode.OK).send({ success: true, result: data });
+        break;
+      case POST:
+        break;
+      case PUT:
+        if (!query.id || !body) return methodNotSuported();
+
+        break;
+
+      default:
+        methodNotSuported();
+    }
+  } catch (error) {
+    res
+      .status(HttpStatusCode.INTERNAL_SERVER_ERROR)
+      .send({ success: false, message: error });
+  }
+
+  function methodNotSuported() {
+    res
+      .status(HttpStatusCode.METHOD_NOT_ALLOWED)
+      .send({ success: false, message: ["Method not allowed"] });
+  }
 }
